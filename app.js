082e0213087c609f248c222f9b086613c6c956e4
pascalallen <thomaspascalallen@yahoo.com>
2018-04-16 11:27:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,"/html")));
+
+app.listen(7777,function(){
+    console.log("Started listening on port", 7777);
+});
+
+app.post('/signin', function (req, res) {
+  var user_name=req.body.email;
+  var password=req.body.password;
+  if(user_name=='admin' && password=='admin'){
+      res.send('success');
+  }else{ // not necessary
+    res.send('Failure');
+  }
+});
